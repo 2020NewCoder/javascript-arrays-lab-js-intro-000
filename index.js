87334ce7abbe53b050d410a026733927be32321d
KEITH ROGERS <asocialant@gmail.com>
2020-06-28 20:31:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = [] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name){
-  var myNew = kittens[3]=("Ralph")
-}
-destructivelyAppendKitten(myNew);
-
-function destructivelyPrependKitten(){
-  var top = kittens.unshift("Bob");
-}
-destructivelyPrependKitten(top);
-
-function destructivelyRemoveLastKitten(name){
-  var remove =kittens.pop()
-}
-destructivelyRemoveLastKitten(remove);
-
-function destructivelyRemoveFirstKitten(){
-  var gone = kittens.shift()
-}
-destructivelyRemoveFirstKitten();
-
 function appendKitten(name){
-var name = kittens[kittens.push("Broom")];
-return name
+  
 }
-appendKitten(name);
+appendKitten();
