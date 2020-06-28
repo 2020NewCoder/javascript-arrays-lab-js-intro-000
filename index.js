65ff2293c8e6aba1fc620e8859d6061b935370c3
KEITH ROGERS <asocialant@gmail.com>
2020-06-28 20:35:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var kittens = ["Milo", "Otis", "Garfield","Joey", "Ralph", "Bob"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield",] //define your array here
 
 // Add your functions and code here
 function appendKitten(name){
