3974ec6984803627d6210aa986140725658e64b7
KEITH ROGERS <asocialant@gmail.com>
2020-06-28 20:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  var myNew = kittens[3]=("Ralph")
+}
+destructivelyAppendKitten(myNew);
+
+function destructivelyPrependKitten(){
+  var top = kittens.unshift("Bob");
+}
+destructivelyPrependKitten(top);
+
+function destructivelyRemoveLastKitten(name){
+  var remove =kittens.pop()
+}
+destructivelyRemoveLastKitten(remove);
+
+function destructivelyRemoveFirstKitten(){
+  var gone = kittens.shift()
+}
+destructivelyRemoveFirstKitten();
+
+function appendKitten(name){
+var name = kittens[kittens.push("Broom")];
+return name
+}
+appendKitten(name);
