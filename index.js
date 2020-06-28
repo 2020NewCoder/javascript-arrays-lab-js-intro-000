eb0a8a8e671ebf3d02ebb09396d1d4131d84c875
KEITH ROGERS <asocialant@gmail.com>
2020-06-28 20:36:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield",] //define your array here
 // Add your functions and code here
 function appendKitten(name){
 var myBaby = kittens.push("Broom")
-return myBaby
+return kittens
 }
 
 appendKitten();
